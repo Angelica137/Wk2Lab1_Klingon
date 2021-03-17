6a8841dfec249aa3c5e3bb38c3353acee599a48c
Angelica <Angelica137@users.noreply.github.com>
2021-03-17 21:38:55 +0000

    add name and date

--- KlingonShip.java
+++ KlingonShip.java
@@ -2,19 +2,14 @@
 /**
  * AlienShip - Unit 4 exercise.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Angelica Beristain
+ * @version 17 March 2021
  */
-public class KlingonShip
-{
+public class KlingonShip {
 	double photonPower = 45;
 	int threatFactor = 7;
 	double cosmicProximity = 20.6;
-	
-	//Put the displayKlingonDetails()and getDangerLeve() methods below.
-	
-	
-	
-	
-	
+
+	// Put the displayKlingonDetails()and getDangerLeve() methods below.
+
 }
